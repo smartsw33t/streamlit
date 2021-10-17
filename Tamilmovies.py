b82097c3f39f72ed487e8eeef46b401274ed600b
Jayaseelan Samuel <79314753+smartsw33t@users.noreply.github.com>
2021-10-17 10:04:17 +0530

    Update Tamilmovies.py

--- Tamilmovies.py
+++ Tamilmovies.py
@@ -16,17 +16,17 @@ df =pd.read_csv("Top100.csv")
 
 #method 1
 
-#st.dataframe(df)
+st.dataframe(df)
 
 
 #method 2 static table
 
-st.table(df)
+#st.table(df)
 
 
 #Adding a color style  from pandas
 
-st.dataframe(df.style.highlight_max(axis=0))
+#st.dataframe(df.style.highlight_max(axis=0))
 
 
 
