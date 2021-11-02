72fb41098bfe689b69aa3ab91ae8ca3dbcacfc90
Jayaseelan Samuel <79314753+smartsw33t@users.noreply.github.com>
2021-11-02 21:23:56 +0530

    Update Tamilmovies.py

--- Tamilmovies.py
+++ Tamilmovies.py
@@ -35,33 +35,3 @@ st.dataframe(df)
 
 
 
-
-
-
-
-#Working with Text
-#st.text("Hello world this is a text")
-#name ="bro"
-#st.text("அருமை {}". format(name))
-
-#Header
-#st.header("This is a header")
-#Sub header
-#st.subheader("This is a subheader")
-#Title
-#st.title("This is a title")
-#Markdown
-#st.markdown("## Trying to figure out mark down")
-
-#Colorful text like Bootstrap
-#st.success("Successful")
-#st.warning("Echarikkai")
-#st.info("Thagavalgal")
-#st.error("Engayo thappu nadandhu irukku")
-
-#Superfunction
-#st.write("##Idhu oru vaakkiyam")
-#st.write(888+143)
-#st.write(dir(st))
-#Help
-#st.help(range)
