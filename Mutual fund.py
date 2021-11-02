c572419c5a417d93a0aba3aaa4d069e3618c8745
Jayaseelan Samuel <79314753+smartsw33t@users.noreply.github.com>
2021-11-02 21:27:54 +0530

    Add files via upload

---
+++ Mutual fund.py
@@ -0,0 +1,67 @@
+#Basics and fundamentals
+
+
+#Core package
+import streamlit as st
+
+
+# Load EDA packages
+
+import pandas as pd 
+
+#display data
+st.header("Best Mutual Funds")
+
+df =pd.read_excel("MF.xlsx")
+
+#method 1
+
+#st.dataframe(df)
+
+
+#method 2 static table
+
+st.table(df)
+
+
+#Adding a color style  from pandas
+
+st.dataframe(df.style.highlight_max(axis=0))
+
+
+
+
+
+
+
+
+
+
+
+
+#Working with Text
+#st.text("Hello world this is a text")
+#name ="bro"
+#st.text("அருமை {}". format(name))
+
+#Header
+#st.header("This is a header")
+#Sub header
+#st.subheader("This is a subheader")
+#Title
+#st.title("This is a title")
+#Markdown
+#st.markdown("## Trying to figure out mark down")
+
+#Colorful text like Bootstrap
+#st.success("Successful")
+#st.warning("Echarikkai")
+#st.info("Thagavalgal")
+#st.error("Engayo thappu nadandhu irukku")
+
+#Superfunction
+#st.write("##Idhu oru vaakkiyam")
+#st.write(888+143)
+#st.write(dir(st))
+#Help
+#st.help(range)
